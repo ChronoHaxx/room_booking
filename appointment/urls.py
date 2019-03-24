6a97c14829067a6d695739f5adb5c7055f8835d4
ChronoHax <mhazimiskp@gmail.com>
2019-03-25 00:42:11 +0800

    Goodnight.

--- appointment/urls.py
+++ appointment/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('view/', views.index)
+    path('view/<year>/<month>/<day>', views.index)
 ]
--- appointment/views.py
+++ appointment/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+from appointment.models import room1, room2, room3
 
-def index(request):
-    return render(request,'index.html')
+def index(request,year,month,day):
+    room1new = room1.objects.all().filter(start_time__year=year, 
+                                        start_time__month=month, 
+                                        start_time__day=day)
+
+    room2new = room2.objects.all().filter(start_time__year=year, 
+                                        start_time__month=month, 
+                                        start_time__day=day)
+
+    room3new = room3.objects.all().filter(start_time__year=year, 
+                                        start_time__month=month, 
+                                        start_time__day=day)
+
+    return render(request,'index.html',context={"room1" : room1new,
+                                                "room2" : room2new,
+                                                "room3" : room3new})
+
