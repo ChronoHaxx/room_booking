6d9a2c8d789b23dc3606a58c6582152437282c19
ChronoHax <mhazimiskp@gmail.com>
2019-04-09 01:18:31 +0800

    !!!index view timetable for appointments only, not form input view yet!!!json data from appointment object model successfully to timetable.js and looped and appended to html in basic form.

--- appointment/static/appointment/timetable.js
+++ appointment/static/appointment/timetable.js
@@ -32,46 +32,48 @@ $(document).ready(function() {
     hour_end.push(appointments[k].fields.end_time.split('')[12]);
     hour_end = parseInt(hour_end.join(""));
     hour_end += 8;
+    hour_end_for24h = hour_end
     if (hour_end >= 24){
       hour_end -= 24;
+      hour_end_for24h = "0" + hour_end ;
     };
     minute_end = []
     minute_end.push(appointments[k].fields.end_time.split('')[14]);
     minute_end.push(appointments[k].fields.end_time.split('')[15]);
+    minute_end_for24h = minute_end.join("");
     minute_end = parseInt(minute_end.join(""));
     var total_minute_end = minute_end + (hour_end * 60);
 
     var hour24_start = hour_start_for24h + ':' + minute_start_for24h;
+    var hour24_end = hour_end_for24h + ':' + minute_end_for24h; 
 
     var rooom = appointments[k].fields.rooom
     var durration = total_minute_end - total_minute_start
-    var duration_S = toString(durration)
     console.log(hour24_start)
+    console.log(hour24_end)
     console.log(hour_start)
     console.log(hour_end)
     console.log(rooom)
     console.log(durration)
 
-    $('#datasets').ready(function() {
-      var name = appointments[k].fields.objective;
-      var duration = durration * (5 / 3); /*must be in mins*/
-      var dur = duration_S;
-      var startTime = hour24_start;
-  
-      /*get the hour*/
-      var hour = parseInt(hour24_start) * 100;
-      /*get the minutes*/
-      var min = hour24_start;
-      var res = min.split(":");
-      var minute = parseInt(res[1]) / 60 * 100;
-      var t = hour + parseInt(minute);
-      var screen = parseInt(rooom) * 10;
-      $('.timetable .layoutdesign').append("<span class='film' style='top:" + screen + "vh;left:calc(10vw + " + t + "px); width:" + duration + "px' data-start='" + startTime + "'>" + name + " -"+dur+"mins</span>");
-    });
+
+    var name = appointments[k].fields.objective;
+    var duration = durration * (5 / 3); /*must be in mins*/
+    var dur = durration;
+    var startTime = hour24_start;
+    /*get the hour*/
+    var hour = parseInt(hour24_start) * 100;
+    /*get the minutes*/
+    var min = hour24_start;
+    var res = min.split(":");
+    var minute = parseInt(res[1]) / 60 * 100;
+    var t = hour + parseInt(minute);
+    var screen = parseInt(rooom) * 10;
+    $('.timetable .layoutdesign').append("<span class='film' style='top:" + screen + "vh;left:calc(10vw + " + t + "px); width:" + duration + "px' data-start='" + startTime + "'>" + name  + " | " + hour24_start + " until " + hour24_end + " | " + " -"+dur+"mins " + "</span>");
    }
 
   
-    $('.layoutdesign').click( ".film", function() {
+    $('.layoutdesign').click( ".filmx", function() {
       $(this).toggleClass("activeFilm");
       $('#deleteFilm').show();
     });
