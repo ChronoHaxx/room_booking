96c9dcfdcb64dd88f8645e3d86508b668856019e
ChronoHax <mhazimiskp@gmail.com>
2019-05-31 18:52:26 +0800

    index page done

--- appointment/urls.py
+++ appointment/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('view/<year>/<month>/<day>', views.index, name='appointment-index'),
-    path('view/<year>/<month>/<day>/<pk>/', views.AppointmentDetailView.as_view(), name='appointment-detail'),
-    path('new/', views.add_model, name='appointment-new'),
+    path('view', views.index, name='appointment-index'),
+    path('view/<year>/<month>/<day>', views.table, name='appointment-table'),
+    path('view/<year>/<month>/<day>/<pk>', views.AppointmentDetailView.as_view(), name='appointment-detail'),
+    path('new', views.add_model, name='appointment-new'),
 ]
--- appointment/views.py
+++ appointment/views.py
@@ -6,12 +6,17 @@ from django.core import serializers
 
 from django.utils import timezone
 from django.views.generic.detail import DetailView
-from django import forms
+from django import forms, views
 from .forms import AppointmentForm, MyForm, QuerydateForm
 from django.utils import timezone
 
+#find current date
+year = datetime.date.today().year
+month = datetime.date.today().month
+day = datetime.date.today().day
 
-def index(request,year,month,day):
+#default would go to todays date
+def index(request, year=year, month=month, day=day):
 
     appointments = serializers.serialize('json', Appointment.objects.filter(start_time__year=year, 
                                                             start_time__month=month, 
@@ -31,6 +36,29 @@ def index(request,year,month,day):
     #    durations.append(duration_minute)
 
     return render(request,'index.html',context={"appointments" : appointments,
+                                                #"durations" : durations,
+                                                })
+
+def table(request, year, month, day):
+
+    appointments = serializers.serialize('json', Appointment.objects.filter(start_time__year=year, 
+                                                            start_time__month=month, 
+                                                            start_time__day=day),
+                                                        fields=('objective', 
+                                                                'start_time', 
+                                                                'end_time',
+                                                                'client',
+                                                                'rooom'))    
+
+    #to get duration in minute
+    #durations = []
+    #for x in appointments :
+    #    duration = x.end_time - x.start_time
+    #    duration_array = str(duration).split(':')
+    #    duration_minute = (int(duration_array[0]) * 60) + int(duration_array[1])
+    #    durations.append(duration_minute)
+
+    return render(request,'table.html',context={"appointments" : appointments,
                                                 #"durations" : durations,
                                                 })
 
